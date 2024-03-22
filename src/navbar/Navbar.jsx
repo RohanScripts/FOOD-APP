a9a032ef5fb245ed3a960b28e5e3769a1ec3ce17
RohanScripts <shelarrohan1810@gmail.com>
2024-03-22 23:59:19 +0530

    Add font family and styling to app and navbar

--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NavCss.css";
 import { assets } from "../assets/assets";
 
 const Navbar = () => {
-  return <div></div>;
+  const [menu, setMenu] = useState("Home");
+
+  return (
+    <div className="navbar">
+      <img src={assets.logo} width="95px" alt="" />
+      <ul className="navbar-menu">
+        <li
+          onClick={() => setMenu("Home")}
+          className={menu === "Home" ? "active" : "inactive"}
+        >
+          Home
+        </li>
+        <li
+          onClick={() => setMenu("Menu")}
+          className={menu === "Menu" ? "active" : "inactive"}
+        >
+          Menu
+        </li>
+        <li
+          onClick={() => setMenu("Contact Us")}
+          className={menu === "Contact Us" ? "active" : "inactive"}
+        >
+          Contact Us
+        </li>
+      </ul>
+      <div className="navbar-righside">
+        <img src={assets.search_icon} alt="" />
+        <div className="righside-search-icon">
+          <img src={assets.basket_icon} alt="" />
+          <div className="dot"></div>
+        </div>
+        <button>Sign In</button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
