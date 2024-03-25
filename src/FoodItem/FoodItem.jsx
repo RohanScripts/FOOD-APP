2f585d7624b16e224a0f0749f3ab962b12c51653
RohanScripts <shelarrohan1810@gmail.com>
2024-03-25 13:07:27 +0530

    Refactor FoodItem component and StoreContext

--- src/FoodItem/FoodItem.jsx
+++ src/FoodItem/FoodItem.jsx
@@ -1,31 +1,32 @@
-import React, { useState } from "react";
+import React, { useContext } from "react";
 import "./FoodItemCss.css";
 import { assets } from "../assets/assets";
+import { StoreContext } from "../context/StoreContext";
 
 const FoodItem = ({ id, name, image, price, description }) => {
-  const [itemCount, setItemCount] = useState(0);
+  const { cartItem, addToCart, removeFromCart } = useContext(StoreContext);
 
   return (
     <div className="food-item">
       <div className="food-item-image-container">
         <img className="food-item-image" src={image} alt="" />
-        {itemCount === 0 ? (
+        {!cartItem[id] ? (
           <img
             className="add"
-            onClick={() => setItemCount((prev) => prev + 1)}
+            onClick={() => addToCart(id)}
             src={assets.add_icon_white}
             alt=""
           />
         ) : (
           <div className="food-item-counter">
             <img
-              onClick={() => setItemCount((prev) => prev - 1)}
+              onClick={() => removeFromCart(id)}
               src={assets.remove_icon_red}
               alt=""
             />
-            <p>{itemCount}</p>
+            <p>{cartItem[id]}</p>
             <img
-              onClick={() => setItemCount((prev) => prev + 1)}
+              onClick={() => addToCart(id)}
               src={assets.add_icon_green}
               alt=""
             />
