f4983f401356fa62d1dec47c9ba8501623e0755b
RohanScripts <shelarrohan1810@gmail.com>
2024-03-29 01:27:55 +0530

    Add cart total and promo code section to CartPage

--- src/pages/cart/CartPage.jsx
+++ src/pages/cart/CartPage.jsx
@@ -3,7 +3,8 @@ import "./CartPageCss.css";
 import { StoreContext } from "../../context/StoreContext";
 
 const CartPage = () => {
-  const { cartItem, food_list, removeFromCart } = useContext(StoreContext);
+  const { cartItem, food_list, removeFromCart, getTotalCartAmount } =
+    useContext(StoreContext);
 
   // Check if cartItem is empty
   const isEmptyCart = Object.keys(cartItem).length === 0;
@@ -16,43 +17,76 @@ const CartPage = () => {
           <p class="empty-message">Your cart is empty.</p>
         </div>
       ) : (
-        <div className="cart-items">
-          <div className="cart-item-title">
-            <p>Items</p>
-            <p>Title</p>
-            <p>Price</p>
-            <p>Quantity</p>
-            <p>Total</p>
-            <p>Remove</p>
-          </div>
-          <br />
-          <hr />
-          {food_list.map((item, index) => {
-            if (cartItem[item._id] > 0) {
-              return (
-                <>
-                  <div
-                    className="cart-items-info cart-item-title"
-                    key={item._id}
-                  >
-                    <img src={item.image} alt="" />
-                    <p>{item.name}</p>
-                    <p>{item.price}</p>
-                    <p>{cartItem[item._id]}</p>
-                    <p>{item.price * cartItem[item._id]}</p>
-                    <p
-                      className="cross"
-                      onClick={() => removeFromCart(item._id)}
+        <>
+          <div className="cart-items">
+            <div className="cart-item-title">
+              <p>Items</p>
+              <p>Title</p>
+              <p>Price</p>
+              <p>Quantity</p>
+              <p>Total</p>
+              <p>Remove</p>
+            </div>
+            <br />
+            <hr />
+            {food_list.map((item, index) => {
+              if (cartItem[item._id] > 0) {
+                return (
+                  <>
+                    <div
+                      className="cart-items-info cart-item-title"
+                      key={item._id}
                     >
-                      X
-                    </p>
-                  </div>
-                  <hr />
-                </>
-              );
-            }
-          })}
-        </div>
+                      <img src={item.image} alt="" />
+                      <p>{item.name}</p>
+                      <p>₹{item.price}</p>
+                      <p>{cartItem[item._id]}</p>
+                      <p>₹{item.price * cartItem[item._id]}</p>
+                      <p
+                        className="cross"
+                        onClick={() => removeFromCart(item._id)}
+                      >
+                        X
+                      </p>
+                    </div>
+                    <hr />
+                  </>
+                );
+              }
+            })}
+          </div>
+          <div className="cart-bottom">
+            <div className="cart-total">
+              <h2>Cart Totals</h2>
+              <div>
+                <div className="cart-total-details">
+                  <p>Subtotal</p>
+                  <p>₹{getTotalCartAmount()}</p>
+                </div>
+                <hr />
+                <div className="cart-total-details">
+                  <p>Delivery Fee</p>
+                  <p>₹{55}</p>
+                </div>
+                <hr />
+                <div className="cart-total-details">
+                  <b>Total</b>
+                  <b>₹{getTotalCartAmount() + 55}</b>
+                </div>
+              </div>
+              <button>Proceed To Checkout</button>
+            </div>
+            <div className="cart-promo-section">
+              <div>
+                <p>If you have any promo code, enter it here</p>
+                <div className="promo-code-inputs">
+                  <input type="text" placeholder="promo code..." />
+                  <button>Submit</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
       )}
     </div>
   );
